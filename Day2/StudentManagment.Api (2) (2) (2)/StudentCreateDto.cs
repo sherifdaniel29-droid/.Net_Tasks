75d8e8574178d94a8d79d505a2b13d5a93fc748c
Daniisherif <danysherif2007@gmail.com>
2026-08-23 11:49:10 +0300

    Day1&Day2 folders

---
+++ Day2/StudentManagment.Api (2) (2) (2)/StudentCreateDto.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace StudentManagement.Api.Models;
+public class StudentCreateDto
+{
+    public string Name { get; set; } = string.Empty;
+    public int Age { get; set; }
+    public string DepartmentName { get; set; } = string.Empty;
+}
